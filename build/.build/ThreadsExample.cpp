cccc04444822df4d635d086d9b03dd9d3b167889
Scrappers <scrappers.tm@gmail.com>
2021-07-29 02:53:05 -0500

    Initial Commit

---
+++ build/.build/ThreadsExample.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cstdlib>
+#include <pthread.h>
+#include <unistd.h>
+
+using namespace std;
+
+/**
+* Run method for thread1
+* @param void* arg the thread passed argument
+*/
+void *run1(void* arg) {
+      cout << "Thread 1\n" << endl;
+      //sleep 10 secs
+      sleep(10);
+      pthread_exit(NULL); 
+  return NULL;
+}
+/**
+* Run method for thread2
+* @param void* arg the thread passed argument
+*/
+void *run2(void* arg) {
+      cout << "Thread 2\n" << endl;
+      //sleep 10 secs
+      sleep(10);
+      pthread_exit(NULL); 
+  return NULL;
+}
+/**
+* Run main Thread
+* @return the status of the run
+*/
+int main () {
+   //the main thread is task0
+   cout << "Thread 0\n" << endl;
+   //sleep 10 secs
+   sleep(10);
+   int id1 = '1';
+   //create thread 1
+   pthread_t* thread1 = new pthread_t();
+   //start thread 1
+   int state1 = pthread_create(thread1, nullptr, run1, &id1); 
+   void** status1 = NULL;
+   //make the thread wait until the execution of the previous one
+   pthread_join(*thread1, status1);
+   int id2 = '2';
+   //create thread 2
+   pthread_t* thread2 = new pthread_t();
+   //start thread 2
+   int state2 = pthread_create(thread2, NULL, run2, &id2); 
+   void** status2 = NULL;
+   //make the thread wait until the execution of the previous one
+   pthread_join(*thread2, status2);   
+   //exit the main thread to register others into the Processors Registers
+   pthread_exit(NULL);
+ return 0;
+}
---
+++ build/compile.sh
@@ -0,0 +1,56 @@
+#**
+#* Ccoffee Build tool, manual build, alpha-v1.
+#* Custom Includsions for GTKmm cpp wrapper
+#* dependencies '-I"/usr/include/glibmm-2.9.1/glib" -I"/usr/include/sigc++-2.0/sigc++" -I"/usr/include/giomm-2.4" -I"/usr/include/gtkmm-4.2.0/gtk"'
+#*
+#* @author pavl_g.
+#*#
+echo "Compiling the project"
+
+#1) define work directory
+workDir='/home/twisted/GradleProjects/CThreads'
+
+#2) delete old build
+oldbuild=(${workDir}'/build/.build/*')
+for ((idx=0; idx < ${#oldbuild[@]}; idx++)); do
+    rm ${oldbuild[$idx]}
+done
+
+##remove the dir
+rmdir ${workDir}'/build/.build/'
+
+#3) recreate a clean one to gather cpp files for compiling & linking
+mkdir ${workDir}'/build/.build'
+
+##attrs : dir to compile & sharedLib name
+libs=(${workDir}'/src/libs/*')
+main=(${workDir}'/src/main/*')
+
+merge[0]=${libs}
+merge[1]=${main}
+
+#4) copy cpp files to a gather directory
+for ((idx=0; idx < ${#merge[@]}; idx++)); do
+##act on ${merge[$idx]}
+    cp ${merge[$idx]} ${workDir}'/build/.build'
+done
+
+# 5) get the final String
+final=${workDir}'/build/.build/*'
+
+# 6) execute using g++ compiler against cpp code
+##prepare exec names
+sharedlib='ThreadsTest'
+executable='ThreadsTest.exec'
+# 7) compile files with inclusions
+g++ -x c++ -I${workDir}'/src/includes' -I'/usr/include' -o ${sharedlib} ${final} -lpthread
+clang++ -x c++ -I${workDir}'/src/includes' -I'/usr/include' -o ${executable} ${final} -lpthread
+# 7) move files to output dir
+##prepare dirs
+builddir=${workDir}'/build/'
+outputdir=${workDir}'/output/'
+##moving files
+mv ${builddir}${sharedlib} ${outputdir}${sharedlib}
+mv ${builddir}${executable} ${outputdir}${executable}
+
+echo "Successfully Compiled"
